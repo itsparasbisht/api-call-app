3850eea9e9b0e1f839e202f091353ef8d8f78f9f
itsparasbisht <iamparasbisht@gmail.com>
2022-11-12 20:28:09 +0530

    allow tab on textarea

--- setupEditor.js
+++ setupEditor.js
@@ -1,11 +1,19 @@
 import Yace from "yace";
 
 const editor = new Yace("[data-json-request-body]", {
-  value: "{\n\t\n}",
+  value: "",
   lineNumbers: true,
 });
 
-editor.update({ value: data });
+const textarea = document.querySelector("textarea");
+textarea.addEventListener("keydown", (e) => {
+  if (e.keyCode == 9) {
+    e.preventDefault();
+    e.target.value += "\t";
+  }
+});
+
+// editor.update({ value: data });
 
 // editor.onUpdate((value) => console.log(`new value: ${value}`));
 // editor.destroy();
