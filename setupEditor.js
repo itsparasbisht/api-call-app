4a7b6391ea7d393f7ebc77199e1dbaca5139b970
itsparasbisht <iamparasbisht@gmail.com>
2022-11-13 18:41:00 +0530

    req, res editor update

--- setupEditor.js
+++ setupEditor.js
@@ -1,15 +1,4 @@
-import Yace from "yace";
-
-const responseEditor = new Yace("[data-json-response-body]", {
-  value: "",
-  lineNumbers: false,
-});
-
-export let requestJSON = "";
-
-export function updateResponseEditor(data) {
-  responseEditor.update({ value: JSON.stringify(data, null, 2) });
-}
+// request editor ----------------------
 
 let requestEditorElement = document.querySelector("[data-json-request-body]");
 
@@ -21,3 +10,15 @@ requestEditor.on("change", (editor) => {
   const text = editor.doc.getValue();
   requestJSON = text;
 });
+
+// response editor ----------------------
+
+let responseEditorElement = document.querySelector("[data-json-response-body]");
+let responseJSON = "";
+
+export function updateResponseEditor(data) {
+  responseEditorElement.innerHTML = "";
+  const pEl = document.createElement("p");
+  pEl.innerHTML = "<pre>" + JSON.stringify(data, null, 2) + "</pre>";
+  responseEditorElement.appendChild(pEl);
+}
